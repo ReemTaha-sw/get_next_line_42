0afd5eab1d03c1eb87490c6121ad7dccaa4ea8d2
Reem Taha <62940425+ReemTaha-sw@users.noreply.github.com>
2023-09-08 11:43:51 +0000

    adding bonus

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -86,15 +86,15 @@ char	*extract_line(char *buffer)
 char	*get_next_line(int fd)
 {
 	char		*line;
-	static char	*buffer;
+	static char	*buffer[1024];
 
 	if (fd < 0 || BUFFER_SIZE <= 0 || BUFFER_SIZE >= 2147483647)
 		return (NULL);
-	buffer = read_line(fd, buffer);
-	if (!buffer)
+	buffer[fd] = read_line(fd, buffer[fd]);
+	if (!buffer[fd])
 		return (NULL);
-	line = extract_line(buffer);
-	buffer = skip_line(buffer);
+	line = extract_line(buffer[fd]);
+	buffer[fd] = skip_line(buffer[fd]);
 	return (line);
 }
 
@@ -102,18 +102,31 @@ int	main(void)
 {
 	char	*line;
 	int		i;
-	int		fd;
+	int		fd1;
+	int		fd2;
+	int		fd3;
 
-	fd = open("jj.txt", O_RDONLY);
-	printf("\n\n=====  get next line one file  =====\n\n");
+	fd1 = open("jj.txt", O_RDONLY);
+	fd2 = open("jj1.txt", O_RDONLY);
+	fd3 = open("jj2.txt", O_RDONLY);
+	printf("\n\n=====  get next line from one file  =====\n\n");
 	i = 1;
 	while (i < 3)
 	{
-		line = get_next_line(fd);
-		printf("file :: line %d: ---------->  %s", i, line);
+		line = get_next_line(fd1);
+		printf("line %d: 			%s", i, line);
+		free(line);
+		line = get_next_line(fd2);
+		printf("line %d: 			%s", i, line);
+		free(line);
+		line = get_next_line(fd3);
+		printf("line %d: 			%s", i, line);
+		free(line);
 		i++;
 	}
-	printf("\n\n=====  colseing get next line  =====\n\n");
-	close(fd);
+	printf("\n\n=====  colseing get next line with multibles files  =====\n\n");
+	close(fd1);
+	close(fd2);
+	close(fd3);
 	return (0);
 }
---
+++ get_next_line_bonus.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rosman <rosman@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/02 21:41:33 by hankhali          #+#    #+#             */
+/*   Updated: 2023/09/07 19:33:01 by rosman           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 2
+# endif
+
+char	*ft_strjoin(char *string1, char *string2);
+int		ft_strlen(const char *str);
+char	*ft_strchr(const char *str, int c);
+char	*ft_strdup(const char *s1);
+char	*read_line(int fd, char *str);
+char	*skip_line(char *input_string);
+char	*extract_line(char *input_str);
+char	*get_next_line(int fd);
+
+#endif
